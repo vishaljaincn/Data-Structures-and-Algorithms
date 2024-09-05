c88c5085e4f123968e88a6a5c070bb11320e031d
vishaljain <vishaljaincn@gmail.com>
2024-09-06 01:50:27 +0530

    new graph code added

--- day78_Advanced_Graphs_3_MST_Dijkstra/Dijkstras_Algorithm_Shortest_Path_Priority_Queue_New_Variant.java
+++ day78_Advanced_Graphs_3_MST_Dijkstra/Dijkstras_Algorithm_Shortest_Path_Priority_Queue_New_Variant.java
@@ -1,61 +1,54 @@
 package day78_Advanced_Graphs_3_MST_Dijkstra;
-/*
-Given a weighted, undirected, and connected graph of V vertices and an adjacency list adj where adj[i] is a list of
-lists containing two integers where the first integer of each list j denotes there is an edge between i and j, second
-integers corresponds to the weight of that edge. You are given the source vertex S and You have to Find the shortest
-distance of all the vertex from the source vertex S. You have to return a list of integers denoting the shortest distance
-between each node and the Source vertex S.
-Note: The Graph does not contain any negative weight cycle.
-Example 1:
-Input:
-V = 2
-adj [] = {{{1, 9}}, {{0, 9}}}
-S = 0
-Output:
-0 9
-Explanation:
-The source vertex is 0. Hence, the shortest distance of node 0 from the source is 0 and the shortest distance of node 1
-from source will be 9.
- */
-/*
-Time Complexity: O( E log(V) ), Where E = Number of edges and V = Number of Nodes.
-
-Space Complexity: O( |E| + |V| ), Where E = Number of edges and V = Number of Nodes.
- */
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
+/*
+Given a weighted, undirected, and connected graph of V vertices and an adjacency list adj where adj[i] is a list of lists
+containing two integers where the first integer of each list j denotes there is an edge between i and j, and the second
+integer corresponds to the weight of that edge. You are given two vertices A and B, and you have to find the shortest
+distance between them using Dijkstra's algorithm. The graph does not contain any negative weight cycles.
+
+Example 1:
+Input:
+V = 5
+adj = {{{1, 2}, {2, 4}}, {{0, 2}, {3, 3}}, {{0, 4}, {4, 1}}, {{1, 3}, {4, 5}}, {{2, 1}, {3, 5}}}
+A = 0
+B = 4
+Output:
+Shortest distance from node 0 to node 4 is 5
+*/
 
 // Class representing a pair of vertex and its weight
-class Pairs {
+class Pairss {
     int vertex;
     int weight;
 
-    Pairs(int vertex, int weight) {
+    Pairss(int vertex, int weight) {
         this.vertex = vertex;
         this.weight = weight;
     }
 }
 
 // Main solution class
-public class Dijkstras_Algorithm_Shortest_Path_Priority_Queue {
+public class Dijkstras_Algorithm_Shortest_Path_Priority_Queue_New_Variant {
 
     // Main method
     public static void main(String[] args) {
         // Example usage
-        Dijkstras_Algorithm_Shortest_Path_Priority_Queue solution = new Dijkstras_Algorithm_Shortest_Path_Priority_Queue();
+        Dijkstras_Algorithm_Shortest_Path_Priority_Queue_New_Variant solution = new Dijkstras_Algorithm_Shortest_Path_Priority_Queue_New_Variant();
         int A = 5;
         int[][] B = {{0, 1, 2}, {0, 2, 4}, {1, 3, 3}, {2, 4, 1}, {3, 4, 5}};
-        int C = 0;
-        int[] result = solution.solve(A, B, C);
+        int startNode = 0;
+        int endNode = 4;
+        int shortestDistance = solution.findShortestDistance(A, B, startNode, endNode);
 
-        // Print the result
-        System.out.println(Arrays.toString(result));
+        // Print the shortest distance
+        System.out.println("Shortest distance from node " + startNode + " to node " + endNode + " is: " + shortestDistance);
     }
 
-    // Function to find the shortest distances from a source node
-    public int[] solve(int A, int[][] B, int C) {
+    // Function to find the shortest distance between two nodes A and B
+    public int findShortestDistance(int A, int[][] B, int startNode, int endNode) {
 
         // Adjacency list using ArrayList for flexibility
         ArrayList<ArrayList<Pairss>> adj = new ArrayList<>();
@@ -79,15 +72,20 @@ public class Dijkstras_Algorithm_Shortest_Path_Priority_Queue {
         // Priority queue to store vertices with their distances
         PriorityQueue<Pairss> pq = new PriorityQueue<>((x, y) -> x.weight - y.weight);
 
-        // Start from the source node C
-        distances[C] = 0;
-        pq.offer(new Pairss(C, 0));
+        // Start from the startNode
+        distances[startNode] = 0;
+        pq.offer(new Pairss(startNode, 0));
 
         // Dijkstra's algorithm
         while (!pq.isEmpty()) {
             Pairss current = pq.poll();
             int currentNode = current.vertex;
             int currentDistance = current.weight;
+
+            // If we reached the target node, return the distance
+            if (currentNode == endNode) {
+                return currentDistance;
+            }
 
             // Update distances for adjacent nodes
             for (Pairss neighbor : adj.get(currentNode)) {
@@ -103,14 +101,7 @@ public class Dijkstras_Algorithm_Shortest_Path_Priority_Queue {
             }
         }
 
-        // Convert any unreachable nodes to -1
-        for (int i = 0; i < A; i++) {
-            if (distances[i] == Integer.MAX_VALUE) {
-                distances[i] = -1;
-            }
-        }
-
-        // Return the array of distances
-        return distances;
+        // If the endNode is unreachable, return -1
+        return -1;
     }
 }
